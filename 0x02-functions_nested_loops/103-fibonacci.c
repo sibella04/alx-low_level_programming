54c2b74f4ce81ca6cd127d1e8a2dfefb529a0622
sibella <sibella_arlanda@hotmail.com>
2022-09-14 13:04:57 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
-	
+
 	while (n3 <= 4000000)
 	{
 		n3 = n1 + n2;
 		n1 = n2;
 		n2 = n3;
-		
+	
 		if ((n1 % 2) == 0)
 		sum += n1;
 	}
