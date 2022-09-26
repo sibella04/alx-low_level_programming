91533798e942a53051f8fd91b3ea83a90ef987fd
sibella <sibella_arlanda@hotmail.com>
2022-09-26 12:28:07 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,44 +3,24 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: string to be searched
- * @accept: string to be used
+ * @s: string to check
+ * @accept: string to check against
  *
- * Return: number of bytes in the initial segment of s which are part of accept
+ * Return: number of bytes of s in accept
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	int match = 0;
+	unsigned int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i]; i++)
 	{
-		if (_strchr(accept, s[i]) == NULL)
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
 			break;
-		match++;
-		i++;
 	}
-
-	return (match);
-}
-
-/**
- * _strchr - locates a char in a string
- * @s: string to be searched
- * @c: char to be checked
- *
- * Return: pointer to the first occurence of c in s
- */
-
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	for (; s[i] != c && s[i] != '\0'; i++);
-
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+	return (i);
 }
